7d14a9713ec55aebf623d433f4b9fca2a1ba0fab
qmaxquique <qmax.mail@gmail.com>
2015-01-02 16:38:15 -0300

    output write removed

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 cd ~/
-service apache2 start
 if [ -a .repository/Snapfile ]; then
-    installsnap < /dev/null > /var/www/html/output.stdout 2> /var/www/html/output.stderr snap .repository/Snapfile
-    echo $? > /var/www/html/output.exitCode
+    installsnap snap .repository/Snapfile
 elif [ -a .repository/Dockerfile ]; then
-    installdocker < /dev/null > /var/www/html/output.stdout 2> /var/www/html/output.stderr snap .repository/Dockerfile
-    echo $? > /var/www/html/output.exitCode
+    installdocker snap .repository/Dockerfile
 fi
 . ~/.bashrc
