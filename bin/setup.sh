f2e10b87232a81f5441a4464b102dcecda4b4d8a
qmaxquique <qmax.mail@gmail.com>
2015-01-02 11:05:01 -0300

    Alt function to use startup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd ~/
+service apache2 start
 if [ -a .repository/Snapfile ]; then
-    installsnap .repository/Snapfile
+    installsnap < /dev/null > /var/www/html/output.stdout 2> /var/www/html/output.stderr snap .repository/Snapfile
+    echo $? > /var/www/html/output.exitCode
 elif [ -a .repository/Dockerfile ]; then
-    installdocker snap .repository/Dockerfile
+    installdocker < /dev/null > /var/www/html/output.stdout 2> /var/www/html/output.stderr snap .repository/Dockerfile
+    echo $? > /var/www/html/output.exitCode
 fi
 . ~/.bashrc
