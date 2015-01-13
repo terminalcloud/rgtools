3080ec184139217f14843a22b5c6485bc103558f
qmaxquique <qmax.mail@gmail.com>
2015-01-12 22:30:55 -0300

    Path fixes and re-format

--- bin/checkout_and_setup.sh
+++ bin/checkout_and_setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo "source /root/.bashrc; checkout.sh $1; setup.sh" | /srv/cloudlabs/scripts/run_in_term.js
+. /root/.bashrc
+/root/.rgtools/bin/checkout.sh $1;
+/root/.rgtools/bin/setup.sh
