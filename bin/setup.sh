394dff80b6e198db3404adb852b560814c492e38
qmaxquique <qmax.mail@gmail.com>
2015-01-13 15:24:14 -0300

    Path fixes and re-format

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,3 +6,6 @@ elif [ -a .repository/Dockerfile ]; then
     /root/.rgtools/bin/installdocker snap .repository/Dockerfile
 fi
 . /root/.bashrc
+cat >> /CL/hooks/startup.sh << EOF
+/srv/cloudlabs/scripts/send_message.sh CLIENTMESSAGE '{"id": "1","data": "source /root/.bashrc; history -c ;clear \n","type": "write_to_term","to": "computer"}';
+EOF
